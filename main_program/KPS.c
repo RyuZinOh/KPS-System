ed36441a6e19edd4e08f326d73925825cb94ebc5
Safal Lama <133653188+Happilli@users.noreply.github.com>
2023-07-25 18:41:32 +0545

    safal FixKPS

--- main_program/KPS.c
+++ main_program/KPS.c
@@ -1,25 +1,39 @@
 #include <conio.h>
 #include <stdio.h>
-void s_view(), s_result(), s_attandance(), s_accounting(), s_result();
- 
+#include <stdlib.h>
+
+void s_view();
+void s_result();
+void s_attandance();
+void s_accounting();
+
 int main() {
     int choice;
-    printf("Student Management System Menu\n1: Attandance Section\n2: Accounting Section\n3: Result Section\n4: View Student Basic Information\n5: Exit");
-    printf("Enter Choice :");
+    printf("Student Management System Menu\n1: Attendance Section\n2: Accounting Section\n3: Result Section\n4: View Student Basic Information\n5: Exit\n");
+    printf("Enter Choice: ");
     scanf("%d", &choice);
-    switch(choice){
-        case 1: s_attandance();
-                break;
-        case 2: s_accounting();
-                break;
-        case 3: s_result();
-                break;
-        case 4: exit();
-                break;
-        case 5: printf("Invalid Selection, Re-enter the choice");
-    } 
 
-    
-    getch();
+    switch (choice) {
+        case 1:
+            s_attandance();
+            break;
+        case 2:
+            s_accounting();
+            break;
+        case 3:
+            s_result();
+            break;
+        case 4:
+            s_view();
+            break;
+        case 5:
+            printf("Exiting the program.\n");
+            exit(0);
+            break;
+        default:
+            printf("Invalid Selection, please re-enter the choice.\n");
+    }
+
+    getch();.
     return 0;
 }
